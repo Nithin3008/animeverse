c3b84689827ba302913129dda52544b35bcf892a
Nithin <nithindevathi@gmail.com>
2023-11-13 11:31:12 +0530

    feat: Only logged in users able to access routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,46 @@ import Signup from "./pages/Signup";
 import Explore from "./pages/Explore";
 import Bookmark from "./pages/Bookmark";
 import Profile from "./pages/Profile";
+import AuthVerify from "./components/AuthVerify";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/Home" element={<Home></Home>}></Route>
+        <Route
+          path="/Home"
+          element={
+            <AuthVerify>
+              <Home></Home>
+            </AuthVerify>
+          }
+        ></Route>
         <Route path="/" element={<Login></Login>}></Route>
         <Route path="/Signup" element={<Signup></Signup>}></Route>
-        <Route path="/Explore" element={<Explore></Explore>}></Route>
-        <Route path="/Bookmark" element={<Bookmark></Bookmark>}></Route>
-        <Route path="/Profile/:profileId" element={<Profile></Profile>}></Route>
+        <Route
+          path="/Explore"
+          element={
+            <AuthVerify>
+              <Explore></Explore>
+            </AuthVerify>
+          }
+        ></Route>
+        <Route
+          path="/Bookmark"
+          element={
+            <AuthVerify>
+              <Bookmark></Bookmark>
+            </AuthVerify>
+          }
+        ></Route>
+        <Route
+          path="/Profile/:profileId"
+          element={
+            <AuthVerify>
+              <Profile></Profile>
+            </AuthVerify>
+          }
+        ></Route>
       </Routes>
     </div>
   );
